cacf6dcb5b06db4ed0158b1e08c1ec04608eec4d
xWesleyMartins <wesley_869@hotmail.com.br>
2022-10-18 17:28:47 -0300

    completando req 2, 3 e 4

--- src/routes/talkRouter.js
+++ src/routes/talkRouter.js
@@ -8,4 +8,34 @@ router.get('/talker', async (_req, res) => {
   return res.status(200).json(result);
 });
 
+router.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const result = await readTalkerData.talkerId(Number(id));
+    if (!result) {
+      return res.status(404).json({ 
+        message: 'Pessoa palestrante não encontrada',
+      });
+    }
+  return res.status(200).json(result);
+});
+
+router.post('/login', (req, res) => {
+  const { email, password } = req.body;
+// valida email
+  const validEmailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!email.match(validEmailFormat)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+}
+// valida PassWord
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (password.length <= 5) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  const result = readTalkerData.tokenLogin(email, password);
+  return res.status(200).json({
+    token: result,
+  });
+});
+
 module.exports = router;
--- src/utils/readTalkerData.js
+++ src/utils/readTalkerData.js
@@ -1,17 +1,46 @@
 const fs = require('fs').promises;
 const path = require('path');
+const crypto = require('crypto');
+
 // midleware
 async function readTalker() {
   try {
-    const data = await fs.readFile(path.resolve(__dirname, '../talker.json'));
-    const result = await JSON.parse(data);
-    console.log(result);
+    // const data = await fs.readFile(path.resolve(__dirname, '../talker.json'));
+    const data = path.resolve(__dirname, '../talker.json');
+    const fsData = await fs.readFile(data);
+    const result = await JSON.parse(fsData);
     return result;
-  } catch (err) {
-    console.log(`Erro ao ler os dados ${err.message}`);
+  } catch (error) {
+    console.log(`Erro ao ler os dados ${error.message}`);
+      // return null; // testando retornando null ao invez do console.log
+  }
+}
+// 2
+ async function talkerId(id) {
+  const result = await readTalker();
+  const findById = result.find((element) => element.id === parseInt(id, 10));
+    return findById;
+}
+// 3
+function tokenLogin(email, password) {
+  const genToken = crypto.randomBytes(8).toString('hex');
+   if (email && password) { 
+    return genToken;
   }
 }
 
+// function validateEmail(email) {
+//   const validEmailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//   if (!email) {
+//     message: 'o campo email é obrigatório'
+//   }
+  
+//   if (!email.match(validEmailFormat)) message: 'o campo email deve ter um email válido';
+// }
+
 module.exports = {
   readTalker,
+  talkerId,
+  tokenLogin,
+  // validateEmail,
 };
