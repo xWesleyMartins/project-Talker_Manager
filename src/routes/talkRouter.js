05e278a060a2ef34cb7ec3b71b7a9523c15ceda4
xWesleyMartins <wesley_869@hotmail.com.br>
2022-10-24 16:47:24 -0300

    completando req5 e req6, feitas outras correçoes.

--- src/routes/talkRouter.js
+++ src/routes/talkRouter.js
@@ -22,7 +22,7 @@ router.get('/talker/:id', async (req, res) => {
 router.post('/login', (req, res) => {
   const { email, password } = req.body;
 // valida email
-  const validEmailFormat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/; // ref: https://stackoverflow.com/questions/2507030/email-validation-using-jquery
+  const validEmailFormat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/; // ref: https://medium.com/shopify-hub/como-validar-cpf-e-cnpj-usando-express%C3%B5es-regulares-regex-com-javascript-60779229455d
   if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   if (!email.match(validEmailFormat)) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
@@ -33,9 +33,35 @@ router.post('/login', (req, res) => {
     return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
   const result = readTalkerData.tokenLogin(email, password);
-  return res.status(200).json({
-    token: result,
-  });
+  return res.status(200).json({ token: result });
 });
 
+router.post('/talker', 
+  readTalkerData.tokenAuthorization,
+  readTalkerData.validPlaceName,
+  readTalkerData.validPlaceAge,
+  readTalkerData.validPlaceTalk,
+  readTalkerData.validPlaceWatchedAt,
+  readTalkerData.validPlaceRate,
+  async (req, res) => {
+  const { name, age, talk } = req.body;
+  const result = await readTalkerData.addNewTalkerData(name, age, talk);
+  console.log(name, age, talk);
+
+  return res.status(201).json(result);
+});
+
+router.put('/talker/:id', 
+  readTalkerData.tokenAuthorization,
+  readTalkerData.validPlaceName,
+  readTalkerData.validPlaceAge,
+  readTalkerData.validPlaceTalk,
+  readTalkerData.validPlaceWatchedAt,
+  readTalkerData.validPlaceRate,
+  async (req, res) => {
+  const { name, age, talk } = req.body;
+  const { id } = req.params;
+  const result = await readTalkerData.editForId(id, name, age, talk);
+  return res.status(200).json(result);
+});
 module.exports = router;
--- src/utils/readTalkerData.js
+++ src/utils/readTalkerData.js
@@ -1,6 +1,8 @@
 const fs = require('fs').promises;
 const path = require('path');
 const crypto = require('crypto');
+const { join } = require('path');
+// const talkerJson = require('../talker.json');
 
 // midleware
 async function readTalker() {
@@ -12,7 +14,15 @@ async function readTalker() {
     return result;
   } catch (error) {
     console.log(`Erro ao ler os dados ${error.message}`);
-      // return null; // testando retornando null ao invez do console.log
+  }
+}
+
+async function writeTalkers(newPersonTalker) {
+  try {
+    await fs.writeFile(join(__dirname, '../talker.json'), JSON.stringify(newPersonTalker));
+  } catch (error) {
+    console.log('erro ao escrever arquivo', error.message);
+    return null;
   }
 }
 // 2
@@ -28,19 +38,87 @@ function tokenLogin(email, password) {
     return genToken;
   }
 }
+// - 5 
+function tokenAuthorization(req, res, next) {
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
+  if (authorization.length !== 16) return res.status(401).json({ message: 'Token inválido' });
+  next();
+}
+function validPlaceName(req, res, next) {
+  const { name } = req.body;
+  if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  if (name.length <= 2) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+}
+function validPlaceAge(req, res, next) {
+  const { age } = req.body;
+  if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+}
+function validPlaceTalk(req, res, next) {
+  const { talk } = req.body;
+  if (!talk) return res.status(400).json({ message: 'O campo "talk" é obrigatório' });
+  next();
+}
+function validPlaceWatchedAt(req, res, next) {
+  const { watchedAt } = req.body.talk;
+  const dateFormat = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/; // regex ref: https://stackoverflow.com/questions/5465375/javascript-date-regex-dd-mm-yyyy
+  if (!watchedAt) return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
+  if (!watchedAt.match(dateFormat)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  next();
+}
 
-// function validateEmail(email) {
-//   const validEmailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-//   if (!email) {
-//     message: 'o campo email é obrigatório'
-//   }
-  
-//   if (!email.match(validEmailFormat)) message: 'o campo email deve ter um email válido';
-// }
+function validPlaceRate(req, res, next) {
+  const { rate } = req.body.talk;
+  if (rate < 1 || rate > 5) { 
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  if (!rate) return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
+  next();
+}
+// 5º vvv
+  const addNewTalkerData = async (name, age, talk) => {
+    const talkerJson = await readTalker();
+    const id = talkerJson.length + 1;
+    const result = { id, name, age, talk };
+    await writeTalkers([...talkerJson, result]);
+    return result;
+};
+// ^^^
 
+// 6º vvv
+const editForId = async (id, name, age, talk) => {
+  const talkerJson = await readTalker();
+  const filterId = talkerJson.filter((talker) => Number(talker.id) !== Number(id));
+  const result = { id: Number(id), name, age, talk };
+  await writeTalkers([...filterId, result]);
+  return result;
+};
+// ^^^
 module.exports = {
   readTalker,
   talkerId,
   tokenLogin,
+  // 5º vvvv
+  tokenAuthorization, 
+  validPlaceName,
+  validPlaceAge, 
+  validPlaceTalk,
+  validPlaceWatchedAt,
+  validPlaceRate,
+  writeTalkers,
+  addNewTalkerData,
+  // ^^^^^
+  // 6º vvvv
+  editForId,
+  //   ^^^^
   // validateEmail,
 };
